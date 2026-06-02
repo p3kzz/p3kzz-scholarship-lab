57cd3c5c38ca0a199ae1441c458fb1aebd03b660
sephia <sephiaseptiani4@upi.edu>
2026-06-02 15:59:01 +0700

    feat: add CV upload and parser placeholder endpoint

--- backend/src/controllers/profileController.js
+++ backend/src/controllers/profileController.js
@@ -180,4 +180,67 @@ exports.completeOnboarding = async (req, res) => {
         })
 
     }
-}
+};
+
+exports.uploadCV = async (req, res) => {
+  try {
+
+    if (!req.file) {
+      return res.status(400).json({
+        message: "Upload CV",
+      });
+    }
+
+    const profile = await prisma.profile.update({
+      where: {
+        userId: req.user.userId,
+      },
+
+      data: {
+        cvUrl: `/uploads/cv/${req.file.filename}`,
+        cvParsed: false,
+      },
+    });
+
+    return res.json({
+      success: true,
+      profile,
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    return res.status(500).json({
+      message: error.message,
+    });
+
+  }
+};
+
+exports.parseCV = async (req, res) => {
+
+  try {
+
+    const profile =
+      await prisma.profile.findUnique({
+        where: {
+          userId: req.user.userId,
+        },
+      });
+
+    return res.json({
+      success: true,
+      message: "Menunggu endpoint parser AI",
+      cvUrl: profile.cvUrl,
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      message: error.message,
+    });
+
+  }
+
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const prisma = require('./lib/prisma');
 const authController = require('./controllers/authController');
 const profileController = require('./controllers/profileController')
+const uploadCV = require('./middleware/uploadCV');
 const authMiddleware = require('./middleware/auth');
 
 
@@ -14,6 +15,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/uploads', express.static('uploads'));
 
 app.get('/', (req, res) => {
     res.send('API Running');
@@ -43,6 +46,18 @@ app.get('/me', authMiddleware, async (req, res) => {
 
 app.post('/onboarding', authMiddleware, profileController.completeOnboarding)
 
+app.post(
+  '/profile/upload-cv',
+  authMiddleware,
+  uploadCV.single('cv'),
+  profileController.uploadCV
+);
+
+app.get(
+  '/profile/parse-cv',
+  authMiddleware,
+  profileController.parseCV
+);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
